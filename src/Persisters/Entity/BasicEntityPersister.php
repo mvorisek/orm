5a9e171a721d3b099d43d69ad39e0dfe36649923
Michael Voříšek <mvorisek@mvorisek.cz>
2025-05-05 13:13:33 +0200

    BasicEntityPersister: refactor only - add indent

--- src/Persisters/Entity/BasicEntityPersister.php
+++ src/Persisters/Entity/BasicEntityPersister.php
@@ -276,41 +276,41 @@ class BasicEntityPersister implements EntityPersister
         $stmt      = $this->conn->prepare($this->getInsertSQL());
         $tableName = $this->class->getTableName();
 
-        foreach ($this->queuedInserts as $key => $entity) {
-            $insertData = $this->prepareInsertData($entity);
+            foreach ($this->queuedInserts as $key => $entity) {
+                $insertData = $this->prepareInsertData($entity);
 
-            if (isset($insertData[$tableName])) {
-                $paramIndex = 1;
+                if (isset($insertData[$tableName])) {
+                    $paramIndex = 1;
 
-                foreach ($insertData[$tableName] as $column => $value) {
-                    $stmt->bindValue($paramIndex++, $value, $this->columnTypes[$column]);
+                    foreach ($insertData[$tableName] as $column => $value) {
+                        $stmt->bindValue($paramIndex++, $value, $this->columnTypes[$column]);
+                    }
                 }
-            }
 
             $stmt->executeStatement();
 
-            if ($isPostInsertId) {
-                $generatedId = $idGenerator->generateId($this->em, $entity);
-                $id          = [$this->class->identifier[0] => $generatedId];
+                if ($isPostInsertId) {
+                    $generatedId = $idGenerator->generateId($this->em, $entity);
+                    $id          = [$this->class->identifier[0] => $generatedId];
 
-                $uow->assignPostInsertId($entity, $generatedId);
-            } else {
-                $id = $this->class->getIdentifierValues($entity);
+                    $uow->assignPostInsertId($entity, $generatedId);
+                } else {
+                    $id = $this->class->getIdentifierValues($entity);
+                }
+
+                if ($this->class->requiresFetchAfterChange) {
+                    $this->assignDefaultVersionAndUpsertableValues($entity, $id);
+                }
+
+                // Unset this queued insert, so that the prepareUpdateData() method (called via prepareInsertData() method)
+                // knows right away (for the next entity already) that the current entity has been written to the database
+                // and no extra updates need to be scheduled to refer to it.
+                //
+                // In \Doctrine\ORM\UnitOfWork::executeInserts(), the UoW already removed entities
+                // from its own list (\Doctrine\ORM\UnitOfWork::$entityInsertions) right after they
+                // were given to our addInsert() method.
+                unset($this->queuedInserts[$key]);
             }
-
-            if ($this->class->requiresFetchAfterChange) {
-                $this->assignDefaultVersionAndUpsertableValues($entity, $id);
-            }
-
-            // Unset this queued insert, so that the prepareUpdateData() method (called via prepareInsertData() method)
-            // knows right away (for the next entity already) that the current entity has been written to the database
-            // and no extra updates need to be scheduled to refer to it.
-            //
-            // In \Doctrine\ORM\UnitOfWork::executeInserts(), the UoW already removed entities
-            // from its own list (\Doctrine\ORM\UnitOfWork::$entityInsertions) right after they
-            // were given to our addInsert() method.
-            unset($this->queuedInserts[$key]);
-        }
     }
 
     /**
